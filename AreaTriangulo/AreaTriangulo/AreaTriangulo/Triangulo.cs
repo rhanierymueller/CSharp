0047c0495a40f6eddfe59113c6fb7c2dcf658395
Rhaniery Mueller <rhanimueller@hotmail.com>
2020-06-16 23:32:41 -0300

    Programa para calcular a área de um triângulo

---
+++ AreaTriangulo/AreaTriangulo/AreaTriangulo/Triangulo.cs
@@ -0,0 +1,10 @@
+﻿namespace AreaTriangulo
+{
+    class Triangulo
+    {
+        //public significa que esses atributos podem ser acessados em outros arquivos
+        public double A;
+        public double B;
+        public double C;
+    }
+}

c6487aad361439e2396ef9c2e6dfd83d20827b0b
Rhaniery Mueller <rhanimueller@hotmail.com>
2020-06-17 01:54:21 -0300

    Primeiro Programa

---
+++ PrimeiroPrograma/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,19 @@
+﻿// referencência para o namespace System 
+using System;
+
+//Namespace é um agrupamento de classes relacionadas
+namespace Primeiro
+{
+    //Classe é aunidade básica da programação orientação a objetos
+    class Program
+    {
+        //Aqui podemos ter membros da classe
+        
+        //EntrePoint -> point de entrada para saber aonde a execução começa
+        static void Main(string[] args)
+        {
+            //Para usar o comando Console precisa usar o namespace System, pois ele pertence ao System.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
